362952fc1ecafe33ef8eb44999e2c71dc852d1b4
LeilaRezaei <leilarezaei.prog@gmail.com>
2023-07-09 18:06:35 +0330

    #4 Edit Validation RegisterIdAttribute

--- EducationalInstitution/EducationalInstitution.Api/Validations/RegisterIdAttribute.cs
+++ EducationalInstitution/EducationalInstitution.Api/Validations/RegisterIdAttribute.cs
@@ -6,40 +6,34 @@ namespace EducationalInstitution.Api.Validations
     public class RegisterIdAttribute : ValidationAttribute
     {
        
-        private readonly HashSet<string> RegisteredIds = new HashSet<string>();
+        private readonly HashSet<string> RegisterIds = new HashSet<string>();
 
         public override bool IsValid(object value)
         {
-            if (value == null)
+            if (value == null) 
             {
                 return true;
             }
-
             Regex regexRegisterId = new Regex(@"^\d{4}$");
             if (!regexRegisterId.IsMatch((string)value))
             {
                 return false;
             }
-          
             if (CheckDuplicate((string)value))
             {
                 ErrorMessage = $"Duplicate register id: {(string)value}"; 
                 return false;
             }
-           
-            RegisteredIds.Add((string)value);
+            RegisterIds.Add((string)value);
 
             return true;
         }
-
-       
         private bool CheckDuplicate(string registerId)
         {
-            return RegisteredIds.Any(x => x == registerId);
+            return RegisterIds.Any(x => x == registerId);
         }
        
       
-
         public override string FormatErrorMessage(string name)
         {
             return $"Invalid register id.";
